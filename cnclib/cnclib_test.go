f0f9b8c9c2ec58ec45128edc73c658ca0960929e
ceade <chris.eade@gmail.com>
2022-06-17 22:04:17 +0200

    Add basic test for ToolPath func

--- cnclib/cnclib_test.go
+++ cnclib/cnclib_test.go
@@ -42,3 +42,43 @@ func TestPointInPoly(t *testing.T) {
         t.Log("Point should be in poly")
     }
 }
+
+func TestToolPath(t *testing.T) {
+    // Counter clockwise square (outer toolpath)
+    ccwls := orb.LineString{orb.Point{0,0},orb.Point{10,0},orb.Point{10,10},orb.Point{0,10},orb.Point{0,0}}
+    tp1 := ToolPath(ccwls, 2.0)
+    if tp1[0][0] != -2.0 && tp1[0][1] != -2.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp1[1][0] != 12.0 && tp1[1][1] != -2.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp1[2][0] != 12.0 && tp1[2][1] != 12.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp1[3][0] != -2.0 && tp1[3][1] != 12.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp1[4][0] != -2.0 && tp1[4][1] != -2.0 {
+        t.Log("Toolpath point wrong")
+    }
+
+    // Clockwise square (inner toolpath)
+    cwls := orb.LineString{orb.Point{0,0},orb.Point{0,10},orb.Point{10,10},orb.Point{10,0},orb.Point{0,0}}
+    tp2 := ToolPath(cwls, 2.0)
+    if tp2[0][0] != 2.0 && tp2[0][1] != 2.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp2[1][0] != 2.0 && tp2[1][1] != 8.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp2[2][0] != 8.0 && tp2[2][1] != 8.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp2[3][0] != 8.0 && tp2[3][1] != 2.0 {
+        t.Log("Toolpath point wrong")
+    }
+    if tp2[4][0] != 2.0 && tp2[4][1] != 2.0 {
+        t.Log("Toolpath point wrong")
+    }
+}
