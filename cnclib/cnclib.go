8b7e30bd7611f911d8b21c6e271f318cbf0abe07
ceade <chris.eade@gmail.com>
2022-06-21 11:28:38 +0200

    Add a complete rescan block for picking missed areas
    Since changing the algo to start from where we last were, it's possible to miss
    parts of the shape. This "rescan" segment picks up anything left.

--- cnclib/cnclib.go
+++ cnclib/cnclib.go
@@ -1,30 +1,23 @@
 package cnclib
 
 import (
-        "fmt"
+//        "fmt"
     "math"
     "cnc-tools-go/line2d"
     "github.com/paulmach/orb"
     "github.com/go-gl/mathgl/mgl32"
 )
 
-type TwoPointLine struct {
-    Sx float64
-    Sy float64
-    Ex float64
-    Ey float64
-}
-
 type PolyFillRaster struct {
-    Sx uint32
-    Sy uint32
+    Sx int32
+    Sy int32
     Conv float64
     Raster []uint8
 }
 
 type PolyFillVector struct {
-    Sx uint32
-    Sy uint32
+    Sx int32
+    Sy int32
     Yincr bool
 }
 
@@ -96,11 +89,11 @@ func PointInPoly(x, y float64, ls orb.LineString) bool {
     return c
 }
 
-func polyfillPoint(rst *PolyFillRaster, x, y uint32) orb.Point {
+func polyfillPoint(rst *PolyFillRaster, x, y int32) orb.Point {
     return orb.Point{zify(float64(x) * rst.Conv),zify(float64(y) * rst.Conv)}
 }
 
-func polyfillCanMove(rst *PolyFillRaster, x, y uint32, dx, dy int8) bool {
+func polyfillCanMove(rst *PolyFillRaster, x, y int32, dx, dy int8) bool {
     if dx < 0 {
         if x == 0 {
             return false
@@ -131,7 +124,7 @@ func polyfillCanMove(rst *PolyFillRaster, x, y uint32, dx, dy int8) bool {
     return true
 }
 
-func polyfillMarkAppend(rst *PolyFillRaster, ls orb.LineString, x, y uint32) orb.LineString {
+func polyfillMarkAppend(rst *PolyFillRaster, ls orb.LineString, x, y int32) orb.LineString {
     ls = append(ls, polyfillPoint(rst, x, y))
     rst.Raster[(y * rst.Sx) + x] = 2
     return ls
@@ -209,25 +202,35 @@ func polyfillTracePath(rst *PolyFillRaster, pv *PolyFillVector) orb.LineString {
 }
 
 func polyfillFindNext(rst *PolyFillRaster, pv *PolyFillVector) bool {
-    for x := pv.Sx; x < rst.Sx; x++ {
+    var x, y int32 = 0, 0
+    for x = pv.Sx; x < rst.Sx; x++ {
+        pv.Sx = x
         if pv.Yincr {
-            for y := pv.Sy; y < rst.Sy; y++ {
+            for y = pv.Sy; y < rst.Sy; y++ {
+                pv.Sy = y
                 if rst.Raster[(y * rst.Sx) + x] == 1 {
-                    pv.Sx = x
-                    pv.Sy = y
                     return true
                 }
             }
         } else {
-            for y := pv.Sy; y > 0; y-- {
+            for y = pv.Sy; y >= 0; y-- {
+                pv.Sy = y
                 if rst.Raster[(y * rst.Sx) + x] == 1 {
-                    pv.Sx = x
-                    pv.Sy = y
                     return true
                 }
             }
         }
         pv.Yincr = !pv.Yincr
+    }
+    for x = 0; x < rst.Sx; x++ {
+        for y = 0; y < rst.Sy; y++ {
+            if rst.Raster[(y * rst.Sx) + x] == 1 {
+                pv.Sx = x
+                pv.Sy = y
+                pv.Yincr = true
+                return true
+            }
+        }
     }
     return false
 }
@@ -249,14 +252,12 @@ func PolyFill(ls orb.LineString, toolrad float64) orb.MultiLineString {
     lstr := Translate(ls, 0 - min[0], 0 - min[1])
     //fmt.Printf("min: %0.2f,%0.2f, max: %0.2f,%0.2f\n", min[0], min[1], max[0], max[1])
     var line_sep = (toolrad * 2) * 0.9;
-    rxdim := uint32(math.Round((max[0] - min[0]) / line_sep))
-    rydim := uint32(math.Round((max[1] - min[1]) / line_sep))
     rst := new(PolyFillRaster)
-    rst.Sx = rxdim
-    rst.Sy = rydim
+    rst.Sx = int32(math.Round((max[0] - min[0]) / line_sep))
+    rst.Sy = int32(math.Round((max[1] - min[1]) / line_sep))
     rst.Conv = line_sep
-    rst.Raster = make([]uint8, rxdim * rydim)
-    var y, x uint32
+    rst.Raster = make([]uint8, rst.Sx * rst.Sy)
+    var y, x int32
     for y = 0; y < rst.Sy; y++ {
         for x = 0; x < rst.Sx; x++ {
             if PointInPoly((float64(x) * line_sep), (float64(y) * line_sep), lstr) {
@@ -271,8 +272,9 @@ func PolyFill(ls orb.LineString, toolrad float64) orb.MultiLineString {
     for i := 0; i < len(paths); i++ {
         fin[i] = Translate(paths[i], min[0], min[1])
     }
-
+/*
     for y = 0; y < rst.Sy; y++ {
+        fmt.Print("; ")
         for x = 0; x < rst.Sx; x++ {
             if rst.Raster[(y * rst.Sx) + x] == 1 {
                 fmt.Print("#")
@@ -284,7 +286,7 @@ func PolyFill(ls orb.LineString, toolrad float64) orb.MultiLineString {
         }
         fmt.Print("\n")
     }
-
+*/
     return fin
 }
 
@@ -343,36 +345,6 @@ func PolygonBounds(ls orb.LineString) (orb.Point, orb.Point) {
         }
     }
     return orb.Point{minx, miny}, orb.Point{maxx, maxy}
-}
-
-func line_intersect_point(a TwoPointLine, b TwoPointLine) orb.Point {
-    sxa := a.Sx
-    sya := a.Sy
-    exa := a.Ex
-    eya := a.Ey
-    sxb := b.Sx
-    syb := b.Sy
-    exb := b.Ex
-    eyb := b.Ey
-    if sxa == exa {
-        slb := (eyb - syb) / (exb - sxb)
-        yib := syb - slb * sxb
-        y := slb * exa + yib
-        return orb.Point{zify(exa), zify(y)}
-    }
-    if sxb == exb {
-        sla := (eya - sya) / (exa - sxa)
-        yia := sya - sla * sxa
-        y := sla * sxb + yia
-        return orb.Point{zify(sxb), zify(y)}
-    }
-    sla := (eya - sya) / (exa - sxa)
-    yia := sya - sla * sxa
-    slb := (eyb - syb) / (exb - sxb)
-    yib := syb - slb * sxb
-    x := (yib - yia) / (sla - slb)
-    y := sla * x + yia
-    return orb.Point{zify(x), zify(y)}
 }
 
 func Translate(ls orb.LineString, dx, dy float64) orb.LineString {
